a74cd3db1904630c428626a30a462855a4034059
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-06-18 23:32:03 -0500

    adds conditional  logic

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
 first_name = input("What is your first name? ")
 print("Hello, ", first_name)
-print(first_name, "is learning Python!")
 
+if first_name == "Frank":
+  print(first_name, "is learning Python!")
+  print(first_name + " is the best!")
+elif first_name == "Max":
+  print(first_name + " is learning Python with the community.")
+else: 
+  print("You should totally learn Python {}".format(first_name))
+print("Have a great day {}".format(first_name))
 # print("Hello Frank")
 # print("Hello,",first_name)
 # print("Frank is learning Python.")
