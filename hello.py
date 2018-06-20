86f3cd59de7d8a16b72b8a415eb092f3ba4d9f84
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-06-19 23:41:50 -0500

    comparisons

--- hello.py
+++ hello.py
@@ -7,7 +7,13 @@ if first_name == "Frank":
 elif first_name == "Max":
   print(first_name + " is learning Python with the community.")
 else: 
-  print("You should totally learn Python {}".format(first_name))
+  age = int(input("How old are you? "))
+  # just in case have a younger user who can't read
+  if age <= 6 :
+    print("Maybe you should learn to read first!")
+  else :
+    print("You should totally learn Python {}".format(first_name))
+
 print("Have a great day {}".format(first_name))
 # print("Hello Frank")
 # print("Hello,",first_name)
